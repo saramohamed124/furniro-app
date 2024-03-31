87362ee9a6396467b9b33e56edb45b0d17963649
sarmohamed124 <saramohamaden1234@gmail.com>
2024-03-31 20:16:46 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ export default function App() {
       <Navbar />
         <Routes>
         <Route  path="/furniro-app/" element={<Home />} />
-        <Route  path="/" element={<Home />} />
+        {/* <Route  path="/" element={<Home />} /> */}
         <Route path="/furniro-app/shop" element={<Shop />} />
-        <Route path="shop" element={<Shop />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
+        {/* <Route path="shop" element={<Shop />} /> */}
+        <Route path="/furniro-app/about" element={<About />} />
+        <Route path="/furniro-app/contact" element={<Contact />} />
         </Routes>
         </RecoilRoot>
     </div>
